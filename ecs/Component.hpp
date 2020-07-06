1d9447f6a0dcdd628de56ea86923b0be40349c07
Kevin Garner <kg.dev@protonmail.com>
2020-07-06 12:10:51 -0500

    lowered max number of components

--- ecs/Component.hpp
+++ ecs/Component.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <bitset>
+//#include <limits>
 
 namespace ecs
 {
 	// component bits and maximum number of components
-	const unsigned short MAX_COMPONENTS = USHRT_MAX;
+	const unsigned short MAX_COMPONENTS = 1000;
 	using ComponentBits = std::bitset<MAX_COMPONENTS>;
 
 	class IComponent
