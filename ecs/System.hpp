50e9cf1c039b1c74a701dca4270fe83528e15437
Kevin Garner <kg.dev@protonmail.com>
2020-07-06 12:11:28 -0500

    Added function to ISystem

--- ecs/System.hpp
+++ ecs/System.hpp
@@ -13,6 +13,18 @@ namespace ecs
 	public:
 		virtual void Update() = 0;
 
+		bool EntityHasRequiredComponents(std::bitset<MAX_COMPONENTS> testComponents) const
+		{
+			/* 
+			bitwise AND the test components to the required components.
+			This will 0 out any bit that is either a) not required or b) required but not present, meaning only required bits that are present will be 1
+			at that point. Then, we can get the count of bits that are set. Since only bits that are required will be set, if count() from the result is the same
+			as count() in the required components bitset, we know that the component bits being tested have the required components.
+			*/
+			std::bitset<MAX_COMPONENTS> result = m_requiredComponents & testComponents;
+			return result.count() == m_requiredComponents.count();
+		}
+
 	protected:
 		std::bitset<MAX_COMPONENTS> m_requiredComponents;
 	};
