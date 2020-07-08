ad1253fe3ea29110429a8c84d77c872a8f8ec00d
Kevin Garner <kg.dev@protonmail.com>
2020-07-08 18:50:59 -0500

    Added includes

--- EntityComponentSystem.hpp
+++ EntityComponentSystem.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include "Component.hpp"
+#include "Entity.hpp"
+#include "EntityList.hpp"
+#include "System.hpp"
+
 namespace ecs
 {
 	class EntityComponentSystem
 	{
-	};
+	public:
 
+	protected:
+		
+	};
 }
