65c9a7f681690886b8cb8e421d076d024fcf6850
Kevin Garner <kg.dev@protonmail.com>
2020-07-08 19:33:43 -0500

    removed un-necessary ComponentInternal namespace

--- Component.hpp
+++ Component.hpp
@@ -5,22 +5,17 @@
 
 namespace ecs
 {
-	namespace ComponentInternal
-	{
-		using ComponentType = unsigned short;
-	}
-
 	// component bits and maximum number of components
-	const ComponentInternal::ComponentType MAX_COMPONENTS = 1000;
+	const unsigned short MAX_COMPONENTS = 1000;
 	using ComponentBits = std::bitset<MAX_COMPONENTS>;
 
 	class IComponent
 	{
 	public:
-		virtual inline bool SetComponentBit(ComponentInternal::ComponentType bit) { m_componentBit = bit; }
-		virtual inline ComponentInternal::ComponentType GetComponentBit() const { return m_componentBit; }
+		virtual inline bool SetComponentBit(unsigned short bit) { m_componentBit = bit; }
+		virtual inline unsigned short GetComponentBit() const { return m_componentBit; }
 
 	protected:
-		ComponentInternal::ComponentType m_componentBit;
+		unsigned short m_componentBit;
 	};
 }
