fba17a2ad2a5bfd201c4bd277f2a993669f46426
Kevin Garner <kg.dev@protonmail.com>
2020-07-09 19:27:27 -0500

    added some variables and AddComponent function to ComponentManager

--- ComponentManager.cpp
+++ ComponentManager.cpp
@@ -1,6 +1,27 @@
+#include <type_traits>
+
 #include "ComponentManager.hpp"
 
 namespace ecs
 {
+	template <typename T> bool ComponentManager::AddComponent()
+	{
+		// ensure the component inherits from the component interface class
+		if (!std::is_base_of<IComponent, T>::value)
+		{
+			return false;
+		}
 
+		// if component is already registered
+		if (m_componentNameToBits.count(typeid(T).name()) == 1)
+		{
+			return false;
+		}
+
+		// add component to map of bits
+		m_componentNameToBits.insert({ typeid(T).name() , m_numberOfComponents });
+		++m_numberOfComponents;
+
+		return true;
+	}
 }
--- ComponentManager.hpp
+++ ComponentManager.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <unordered_map>
+
+#include "Component.hpp"
+
 namespace ecs
 {
 	class ComponentManager
 	{
+	public:
+		template <typename T> bool AddComponent();
 
+	protected:
+		// map from typename to a single bit
+		std::unordered_map<const char*, unsigned int> m_componentNameToBits{};
+		unsigned int m_numberOfComponents{};
 	};
 }
